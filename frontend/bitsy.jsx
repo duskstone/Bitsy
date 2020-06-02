89ed0361067e981b5b3a779e95ad951f8a05ef1f
duskstone <jedgardromero@gmail.com>
2020-06-02 17:29:42 -0400

    imported ConfigureStore/added to window for testing

--- frontend/bitsy.jsx
+++ frontend/bitsy.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as sessionAPIUtil from './util/session_api_util.js'
+import configureStore from './store/store';
 
 window.signup = sessionAPIUtil.signup;
 window.login = sessionAPIUtil.login;
 window.logout = sessionAPIUtil.logout;
 
+
 document.addEventListener("DOMContentLoaded", () => {
+        
+    const store = configureStore();
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
+
     const root = document.getElementById("root");
     ReactDOM.render(<h1>Bitsy :3</h1>, root)
 });
