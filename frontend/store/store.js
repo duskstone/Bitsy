0516fe8e794257380b6e9284dc89342f5f043526
duskstone <jedgardromero@gmail.com>
2020-06-02 20:24:38 -0400

    updated store

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -2,8 +2,8 @@ import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
 import rootReducer from "../reducers/root_reducer";
-debugger
 
+debugger
 const configureStore = (preloadedState = {}) => 
     createStore(rootReducer, preloadedState, applyMiddleware(thunk, logger));
 
