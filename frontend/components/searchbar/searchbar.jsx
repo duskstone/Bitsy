de2ecb584d65c1e09992274d2bb1e8de9f2d0fa8
duskstone <jedgardromero@gmail.com>
2020-06-04 14:29:29 -0400

    added presetational componenet for searchbar

---
+++ frontend/components/searchbar/searchbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+    render(){
+        return (
+            <div>
+                <input type="text"
+                className="search-bar"/>
+            </div>
+            
+        )
+    }
+}
+
+export default SearchBar 
