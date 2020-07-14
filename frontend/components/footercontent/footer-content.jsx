b6c4a877f48f1b4c4f38028497357a0e8c62c5a8
duskstone <jedgardromero@gmail.com>
2020-07-14 12:38:44 -0400

    added personal links/icons to footer

--- frontend/components/footercontent/footer-content.jsx
+++ frontend/components/footercontent/footer-content.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { linkedinIcon, githubIcon, angelList, mySite } from "../../../app/assets/images/icons"
 
 class FooterContent extends React.Component {
     render(){
         return (
             <div className="footer-content">
+                <ul className="footer-icons">
+                    <a href="https://www.linkedin.com/in/jonathan-romero-ab325a165/" className="icons linkedin-icon">{linkedinIcon}</a>
+                    <a href="https://github.com/duskstone" className="icons">{githubIcon}</a>
+                    <a href="https://angel.co/u/jonathan-romero-3" className="icons">{angelList}</a>
+                    <a href="https://duskstone.github.io/" className="icons">{mySite}</a>   
+                </ul>
+               
             </div>
             
         )
